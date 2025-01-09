3d5b7de78c3b203cadaefaf414d76063298b03a0
SAIB <muhammadsaibtni@gmail.com>
2025-01-09 11:51:58 +0500

    admin dashboard |saib |31/12/24

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.querySelector('.sidebar');
     const toggleButton = document.querySelector('.toggle-button');
     const toggleIcon = document.getElementById('toggleIcon');
-    const dropdownContent =  document.getElementById('dropdowncontant');
-    const touch = document.getElementById('touch');
-    const slide = document.querySelector('.slide');
-    
+    const collapsibleItems = document.querySelectorAll('.nav-item > a[data-toggle="collapse"]');
+
+    collapsibleItems.forEach((menuItem) => {
+        const dropdown = menuItem.nextElementSibling;    
+        menuItem.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            if (sidebar.classList.contains('collapsed')) {
+                const isVisible = dropdown.style.display === 'block';
+                document.querySelectorAll('.sidebar.collapsed .collapse').forEach((item) => {
+                    item.style.display = 'none';
+                });
+
+                dropdown.style.display = isVisible ? 'none' : 'block';
+
+                if (!isVisible) {
+                    const parentRect = menuItem.getBoundingClientRect();
+                    dropdown.style.top = `${parentRect.top}px`;
+                }
+            }
+        });
+    });
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 768) {
+            sidebar.classList.add('collapsed');
+        } else {
+            sidebar.classList.remove('collapsed');
+        }
+    });
+
     toggles.forEach(toggle => {
         toggle.addEventListener('click', (e) => {
             e.preventDefault();
@@ -14,11 +40,10 @@ document.addEventListener('DOMContentLoaded', function () {
             const targetSelector = toggle.getAttribute('data-target');
             const targetElement = document.querySelector(targetSelector);
     
-            // Toggle the "show" class on the target element
             if (targetElement.classList.contains('show')) {
                 targetElement.classList.remove('show');
             } else {
-                // Close other open dropdowns (optional)
+                
                 const parent = targetElement.getAttribute('data-parent');
                 if (parent) {
                     const parentElement = document.querySelector(parent);
